671974f567fa1ec9c8ae1fbd8c08545d9055f928
ReinMasamune <reinmasamuneytstuff@gmail.com>
2020-01-25 17:50:16 -0500

    Reorganized extensions

---
+++ NetLib/NetLib/Externals/Extensions/External Extensions.cs
@@ -0,0 +1,10 @@
+﻿using BepInEx;
+using System;
+
+namespace NetLib
+{
+    public static class Extensions
+    {
+
+    }
+}
---
+++ NetLib/NetLib/Internals/Extensions/Internal Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine.Networking;
+
+namespace NetLib.Internals.Extensions
+{
+    internal static partial class InternalExtensions
+    {
+        #region NetContext
+        internal static void Write( this NetworkWriter writer, NetContext context )
+        {
+            context.Serialize( writer );
+        }
+        internal static NetContext ReadNetContext( this NetworkReader reader )
+        {
+            return new NetContext( reader );
+        }
+        #endregion
+    }
+}
