9d1131ddbe0f3bce22c72721ac5aaaadc9b3ecfe
ReinMasamune <reinmasamuneytstuff@gmail.com>
2020-01-25 18:36:26 -0500

    Write NetMessageHolder

--- NetLib/NetLib/Internals/Instance Classes/NetMessageHolder.cs
+++ NetLib/NetLib/Internals/Instance Classes/NetMessageHolder.cs
@@ -11,6 +11,7 @@ using UnityEngine.Networking;
 using Mono.Cecil;
 using Mono.Cecil.Rocks;
 using System.Collections.Generic;
+using NetLib.Internals.Extensions;
 
 namespace NetLib.Internals
 {
@@ -21,5 +22,78 @@ namespace NetLib.Internals
         internal BaseNetMethod handler { get; private set; }
         internal NetContext context { get; private set; }
         internal NetMessage message { get; private set; }
+
+        private String typekey = "Invalid";
+
+        public override void Serialize( NetworkWriter writer )
+        {
+            writer.Write( this.safe );
+            if( !this.safe )
+            {
+                Plugin.LogError( "Tries to serialize unsafe message" );
+                return;
+            }
+
+            writer.Write( this.typekey );
+            writer.Write( this.context );
+            this.handler.Serialize( this.message, writer );
+        }
+
+        public override void Deserialize( NetworkReader reader )
+        {
+            this.safe = reader.ReadBoolean();
+            if( !this.safe )
+            {
+                Plugin.LogError( "Recieved unsafe message" );
+                return;
+            }
+
+            this.typekey = reader.ReadString();
+            this.context = reader.ReadNetContext();
+
+            this.handler = MessageLookup.GetNetMethod( this.typekey );
+            if( this.handler == null )
+            {
+                Plugin.LogError( "Stopping deserialization due to unknown message type: " + this.typekey );
+                return;
+            }
+
+            this.message = this.handler.Deserialize( reader );
+        }
+
+        internal NetMessageHolder( NetMessage message, NetContext context )
+        {
+            if( message == null )
+            {
+                Plugin.LogError( "Cannot create NetMessageHolder from null message" );
+                return;
+            }
+            this.message = message;
+
+            if( context == null )
+            {
+                Plugin.LogError( "Cannot create NetMessageHolder from null context" );
+                return;
+            }
+            this.context = context;
+
+            this.typekey = MessageLookup.GetTypeKey( this.message );
+            if( this.typekey == Const.Invalidtypekey )
+            {
+                Plugin.LogError( "Cannot create NetMessageHolder from invalid type" );
+                return;
+            }
+
+            this.handler = MessageLookup.GetNetMethod( this.typekey );
+            if( this.handler == null )
+            {
+                Plugin.LogError( "Cannot create NetMessageHolder from null NetMethod" );
+                return;
+            }
+
+            this.safe = true;
+        }
+
+        public NetMessageHolder() { }
     }
 }
